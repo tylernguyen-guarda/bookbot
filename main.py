67589f36a76a1848ef1a2a35badb36f35dc4cea2
tylernguyen-guarda <tylernguarda@gmail.com>
2025-07-03 11:44:05 -0400

    complete Chapter 2

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from stats import get_number_of_words,  get_number_of_chars
 def get_book_text(path_to_book):
-    with open(path_to_book) as f:
+    with open(path_to_book , encoding="utf-8") as f:
         book_text = f.read()
     return book_text
 
 
 def main():
     book = get_book_text("books/frankenstein.txt")
-    print(f"{get_number_of_words(book)} words found in the document")
-    get_number_of_chars(book)
+    # print(f"{get_number_of_words(book)} words found in the document")
+    print(get_number_of_chars(book))
     
 if __name__  == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -5,7 +5,7 @@ def get_number_of_chars(text):
     char_dict = {}
     for letter in text:
         if char_dict.get(letter.lower()) is None:
-            print(f"found letter '{letter.lower()}'")
-            char_dict.setdefault(letter.lower(),"0")
+            char_dict.setdefault(letter.lower(),1)
+        else:
+            char_dict[letter.lower()] += 1
     return char_dict
-            
