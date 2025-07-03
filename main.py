1bf2f0a40e33755dafc936e002f37ccf3845f41e
tylernguyen-guarda <tylernguyen.guarda@gmail.com>
2025-07-03 12:44:25 -0400

    completed BookBot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from stats import get_number_of_words,  get_number_of_chars
+from stats import get_number_of_words,  get_number_of_chars, chars_dict_to_sorted_list
+import sys
+
 def get_book_text(path_to_book):
     with open(path_to_book , encoding="utf-8") as f:
         book_text = f.read()
@@ -6,9 +8,25 @@ def get_book_text(path_to_book):
 
 
 def main():
-    book = get_book_text("books/frankenstein.txt")
-    # print(f"{get_number_of_words(book)} words found in the document")
-    print(get_number_of_chars(book))
+    #logic for sys args
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path_to_book = sys.argv[1]
     
+    book = get_book_text(path_to_book=path_to_book)
+    # print("============ BOOKBOT ============")
+    # print(f"Analyzing book found at books/frankenstein.txt...")
+    # print("----------- Word Count ----------")
+    print(f"Found {get_number_of_words(book)} total words")
+    
+    # print("--------- Character Count -------")
+    letters_dict = get_number_of_chars(book)
+    
+    sorted_list_of_char_count = chars_dict_to_sorted_list(letters_dict)    
+    for char_info in sorted_list_of_char_count:
+        char = char_info["char"]
+        num = char_info["num"]
+        print(f"{char}: {num}")
 if __name__  == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -9,3 +9,15 @@ def get_number_of_chars(text):
         else:
             char_dict[letter.lower()] += 1
     return char_dict
+
+    # def sort_dict(dictionary):
+    #     return sorted(dictionary.items(), key=lambda x:x[1], reverse=True)
+
+def chars_dict_to_sorted_list(char_dict):
+    char_list = []
+    for char, count in char_dict.items():
+        if char.isalpha():
+            char_list.append({"char":char, "num":count})
+            
+    char_list.sort(key= lambda x: x["num"], reverse=True)
+    return char_list
